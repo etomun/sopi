e8c3ac04fc5776ded95c6aea9040b2a48664e130
etomun <ngadiyantoeto@gmail.com>
2023-12-24 16:36:33 +0700

    Fix flash.sh

--- flash.sh
+++ flash.sh
@@ -13,10 +13,10 @@ if [ -z "$date_time" ]; then
 fi
 
 # For UNIX
-#future_seconds=$(date -j -f "%Y-%m-%d %H:%M:%S" "$date_time" +%s 2>/dev/null)
+future_seconds=$(date -j -f "%Y-%m-%d %H:%M:%S" "$date_time" +%s 2>/dev/null)
 
 # For Linux
-future_seconds=$(date -d "$date_time" +%s 2>/dev/null)
+#future_seconds=$(date -d "$date_time" +%s 2>/dev/null)
 
 # Check if the conversion was successful
 # shellcheck disable=SC2181
@@ -25,22 +25,23 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-current_millis=$(date +%s)
+current_seconds=$(date +%s)
 
 # Convert future_seconds to millis if need
 #future_seconds=$((future_seconds * 1000))
-#current_millis=$((current_millis * 1000))
+#current_seconds=$((current_seconds * 1000))
 
-remaining_seconds=$((future_seconds - current_millis))
+remaining_seconds=$((future_seconds - current_seconds))
 if [ "$remaining_seconds" -lt 0 ]; then
     echo "Expired"
 else
     echo "$remaining_seconds seconds to go"
-    sleep remaining_seconds
+    sleep "$remaining_seconds"
+    echo "Start"
     adb -s localhost shell input tap $((16#0000031e)) $((16#0000089f))
     sleep 1
     adb -s localhost shell input tap $((16#0000031e)) $((16#0000089f))
-    sleep 1
+    sleep 2
     adb -s localhost shell input tap $((16#0000031e)) $((16#0000089f))
     sleep 3
     adb -s localhost shell input keyevent 11
@@ -49,5 +50,8 @@ else
     adb -s localhost shell input keyevent 11
     adb -s localhost shell input keyevent 12
     adb -s localhost shell input keyevent 13
+    echo "Finish"
 fi
+
+echo "End Script"
 
